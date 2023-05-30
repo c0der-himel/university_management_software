3e87786d9d4e898cae27dc395513f493b444acb6
MD. Shahadat Hussain Himel <himel777hussain@gmail.com>
2023-05-30 22:50:13 +0600

    setting up husky

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,11 @@ import mongoose from 'mongoose';
 import app from './app';
 import config from './config/index';
 
-const port: number = 3000;
-
 async function dbConnector(): Promise<void> {
   try {
     await mongoose.connect(config.db_url as string);
     console.log('🍀 DB is connected successfully');
-    app.listen(port, () => {
+    app.listen(config.port, (): void => {
       console.log(`🚀 Server is listening on port ${config.port}`);
     });
   } catch (e) {
