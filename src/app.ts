11119dcf73efbb9b864aa1f66c67f9c541f3d414
MD. Shahadat Hussain Himel <himel777hussain@gmail.com>
2023-06-03 01:14:31 +0600

    auth service part 1 done

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import userRouter from './app/modules/users/users.route';
 
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api/v1/users/', userRouter);
 
 app.get('/', (req: Request, res: Response): void => {
   res.send('Hello World!');
